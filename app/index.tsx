b0d25444bc048d3a949c34d9fdf3f87af88431d4
Arturo Gomez <arturogomezgz04@gmail.com>
2025-12-20 10:01:41 -0700

    refactor authentication screen: replace Input components with LoginCard and remove unused images

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import Input from "../components/input";
+import LoginCard from "../components/loginCard";
 
 export default function Index() {
   return (
@@ -10,11 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hola mundo</Text>
-      <View style={{ width: "90%", marginTop: 20 }}>
-        <Input labelName="Correo" placeholderName="nombre@dominio.com" backgroundColor="#F2F2F2" fontColor="#79747E" password={false} />
-        <Input labelName="Contraseña" placeholderName="********" backgroundColor="#F2F2F2" fontColor="#79747E" password={true} />
-      </View>
+      <LoginCard />
     </View>
   );
 }
---
+++ components/loginCard.tsx
@@ -0,0 +1,35 @@
+import { 
+    StyleSheet,
+    View,
+    Image
+} from "react-native";
+import Input from "./input";
+
+interface Props {
+
+}
+
+export default function LoginCard() {
+    return (
+        <View style={{width: "80%"}}>
+            <View>
+                <Image 
+                    source={require("../assets/images/profileImage.png")} 
+                    style={styles.image} 
+                />
+            </View>
+            <Input labelName="Correo" placeholderName="nombre@dominio.com" backgroundColor="#F2F2F2" fontColor="#79747E" password={false} />
+            <Input labelName="Contraseña" placeholderName="********" backgroundColor="#F2F2F2" fontColor="#79747E" password={true} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 100,
+        height: 100,
+        alignSelf: "center",
+        borderRadius: 50,
+        marginBottom: 20
+    }
+});
